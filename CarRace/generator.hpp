f20368b3d4a24457017f41bceffd30755755b664
SreeragMurali <srmu2001@gmail.com>
2024-02-04 13:00:52 +0530

    CARRACE

---
+++ CarRace/generator.hpp
@@ -0,0 +1,18 @@
+
+//SREERAG MURALEEDHARAN
+
+#pragma once
+#ifndef GENERATOR_HPP
+#define GENERATOR_HPP
+
+#include "line.hpp"
+#include<vector>
+//this is a VECTOR to hold all the lines being created//
+extern std::vector<Line> lines;
+
+//-------LINE GENERATOR-------------//
+
+void generateLines();
+void generateObstacles(sf::Sprite ob[]);
+void generateLife(sf::Sprite lifee);
+#endif // GENERATOR_HPP
